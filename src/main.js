f04c6fa3825bb6a7ab7b1114f2ebe6641fad311b
Ethan-Caracoglia <ethancaracoglia@gmail.com>
2024-10-06 14:13:19 -0400

    Created a proper file structure and began parsing the JSON data

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
 'use strict'
 
-const WEAPON_DATA = 0;
+let WEAPON_DATA;
+let xhr;
+
+// Load the data when the page loads
+window.onload = init;
+
+let init = () => {
+    // Get the json for the weapons
+xhr = new XMLHttpRequest();
+xhr.open('GET', 'data/Helldivers Weapon Stats.json', true);
+xhr.responseType = 'json';
+xhr.onload = () => {
+    if (xhr.status === 200) {
+      let data = xhr.response;
+      WEAPON_DATA = data;
+    }
+  };
+  xhr.send();
+}
